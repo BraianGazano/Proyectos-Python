a71d00b3e230578b78f995762465230b825e45db
Braian <gazanobraian@gmail.com>
2020-12-20 11:36:43 -0300

    Errores solucionados

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 
 class Calculadora():
@@ -10,36 +11,50 @@ class Calculadora():
         self.ventana.resizable(False, False)
         self.operacion = ""
         self.crear_interfaz()
+        self.bloqueo_teclado = False
         self.ventana.mainloop()
 
     def setear_operacion(self, dato, escritura):
-        if not escritura:
-            if dato == "=" and self.operacion != "":
-                # eval: Evalua la expresion de string a int y devuelve el resultado
-                resultado = str(eval(self.operacion))
-                self.operacion = ""
-                self.limpiarPantalla()
-                self.mostrarEnPantalla(resultado)
-            elif dato == "\u232B" and self.operacion != "":
-                self.operacion = str(self.operacion[:len(self.operacion)-1])
-                self.limpiarPantalla()
-                self.mostrarEnPantalla(self.operacion)
-        else:
-            self.operacion += str(dato)
-            self.mostrarEnPantalla(dato)
-        return
+        try:
+            if not escritura:
+                if self.bloqueo_teclado and dato == "\u232B":
+                    self.operacion = ""
+                    self.limpiarPantalla()
+                    self.bloqueo_teclado = False
+                elif dato == "=" and self.operacion != "":
+                    self.limpiarPantalla()
+                    self.operacion = str(eval(self.operacion))
+                    self.mostrarEnPantalla(str(self.operacion))
+                    self.bloqueo_teclado = True
+                elif dato == "\u232B" and self.operacion != "":
+                    self.operacion = str(
+                        self.operacion[:len(self.operacion)-1])
+                    self.limpiarPantalla()
+                    self.mostrarEnPantalla(self.operacion)
+                elif dato == "C" and self.operacion != "":
+                    self.limpiarPantalla()
+                    self.operacion = ""
+            else:
+                self.operacion += str(dato)
+                self.mostrarEnPantalla(dato)
+        except ZeroDivisionError:
+            messagebox.showinfo(
+                'Atención!', 'No se puede realizar la división por 0')
+            self.operacion = ""
+        except SyntaxError:
+            messagebox.showinfo(
+                'Atención!', 'La expresión no se encuentra escrita correctamente')
+            self.operacion = ""
 
     def mostrarEnPantalla(self, valor):
         self.pantalla.configure(state="normal")
         self.pantalla.insert(END, valor)
         self.pantalla.configure(state="disabled")
-        return
 
     def limpiarPantalla(self):
         self.pantalla.configure(state="normal")
         self.pantalla.delete("1.0", END)
         self.pantalla.configure(state="disabled")
-        return
 
     def crear_interfaz(self):
 
@@ -75,6 +90,10 @@ class Calculadora():
                              command=lambda: self.setear_operacion("=", False))
         boton_borrar = Button(self.ventana, text="\u232B",
                               command=lambda: self.setear_operacion("\u232B", False))
+        boton_reiniciar = Button(
+            self.ventana, text="C", command=lambda: self.setear_operacion("C", False))
+        boton_punto = Button(
+            self.ventana, text=".", command=lambda: self.setear_operacion(".", True))
         boton_1.grid(column=0, row=2, pady=7)
         boton_2.grid(column=1, row=2)
         boton_3.grid(column=2, row=2)
@@ -85,12 +104,14 @@ class Calculadora():
         boton_8.grid(column=1, row=4)
         boton_9.grid(column=2, row=4)
         boton_0.grid(column=1, row=5)
+        boton_reiniciar.grid(column=2, row=5)
+        boton_punto.grid(column=0, row=5)
         boton_borrar.grid(column=4, row=2, pady=7)
         boton_suma.grid(column=4, row=3, pady=7)
         boton_resta.grid(column=4, row=4, pady=7)
         boton_multiplicacion.grid(column=4, row=5, pady=7)
         boton_division.grid(column=4, row=6, pady=7)
-        boton_igual.grid(column=0, row=6, columnspan=4)
+        boton_igual.grid(column=0, row=6, columnspan=3)
         boton_igual.config(width=20)
         boton_division.config(width=6)
         boton_multiplicacion.config(width=6)
@@ -107,6 +128,8 @@ class Calculadora():
         boton_7.config(width=3)
         boton_8.config(width=3)
         boton_9.config(width=3)
+        boton_punto.config(width=3)
+        boton_reiniciar.config(width=3)
         self.pantalla = Text(self.ventana, state="disabled", width=20, height=3,
                              background="#D6C30F", foreground="white", font=("Arial", 17))
         self.pantalla.grid(column=0, row=0, columnspan=5)
